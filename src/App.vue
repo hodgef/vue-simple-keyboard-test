a8a47d11cf32b6c30217d96d06efe24c1e91f82b
Francisco Hodge <hello@franciscohodge.com>
2019-07-22 18:19:28 -0400

    initial

---
+++ src/App.vue
@@ -0,0 +1,46 @@
+<template>
+  <div id="app">
+    <input
+      :value="input"
+      class="input"
+      @input="onInputChange"
+      placeholder="Tap on the virtual keyboard to start"
+    >
+    <SimpleKeyboard @onChange="onChange" @onKeyPress="onKeyPress" :input="input"/>
+  </div>
+</template>
+
+<script>
+import SimpleKeyboard from "./components/SimpleKeyboard";
+import "./App.css";
+
+export default {
+  name: "App",
+  components: {
+    SimpleKeyboard
+  },
+  data: () => ({
+    input: ""
+  }),
+  methods: {
+    onChange(input) {
+      this.input = input;
+    },
+    onKeyPress(button) {
+      // eslint-disable-next-line
+      console.log("button", button);
+    },
+    onInputChange(input) {
+      this.input = input.target.value;
+    }
+  }
+};
+</script>
+
+<style>
+#app {
+  font-family: "Avenir", Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+}
+</style>
